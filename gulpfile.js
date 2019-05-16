28cde2f018f4235d5131f49b37e83de6b465b1c7
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-05-16 20:02:54 -0300

    limpando arquivos e css offline

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,40 @@
 const { src, dest, parallel } = require('gulp'),
 prettyHtml = require('gulp-pretty-html'),
-cssbeautify = require('gulp-cssbeautify');
+cssbeautify = require('gulp-cssbeautify'),
+purge = require('gulp-css-purge'),
+  csslint = require('gulp-csslint'),
+concatCss = require('gulp-concat-css');
 
 function html() {
   return src('dist/**/*.html')
     .pipe(prettyHtml({
       indent_size: 2,
       indent_char: ' ',
-      unformatted: ['code', 'pre', 'em', 'strong', 'i', 'b', 'br']
+      unformatted: ['em', 'strong', 'i', 'b', 'br']
     }))
     .pipe(dest('src/'));
 }
 
-function css() {
-  return src('ugly/**/*.css')
-          .pipe(cssbeautify())
-          .pipe(dest('src/css'))
+function csslindo() {
+  return src('src/original/css/*.css')
+    .pipe(cssbeautify({indent: '  '}))
+    .pipe(dest('src/css'))
 }
 
-exports.css = css;
+function cssfeio() {
+  return src('src/css/*.css')
+    .pipe(concatCss("causa.css"))
+    .pipe(dest('src/assets/css'))
+}
+
+function csslinter() {
+  return src('src/css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+}
+
+exports.csslinter = csslinter;
+exports.csslindo = csslindo;
+exports.cssfeio = cssfeio;
 exports.html = html;
-exports.default = parallel(html,css);
+exports.default = parallel(html);
