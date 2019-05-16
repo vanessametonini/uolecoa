5c0c089d292614838bc35b727d317fa63f545c6e
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-05-16 00:47:23 -0300

    Home versão 01

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest, parallel } = require('gulp'),
+prettyHtml = require('gulp-pretty-html'),
+cssbeautify = require('gulp-cssbeautify');
+
+function html() {
+  return src('dist/**/*.html')
+    .pipe(prettyHtml({
+      indent_size: 2,
+      indent_char: ' ',
+      unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br']
+    }))
+    .pipe(dest('src/'));
+}
+
+function css() {
+  return src('ugly/**/*.css')
+          .pipe(cssbeautify())
+          .pipe(dest('src/css'))
+}
+
+exports.css = css;
+exports.html = html;
+exports.default = parallel(html,css);
